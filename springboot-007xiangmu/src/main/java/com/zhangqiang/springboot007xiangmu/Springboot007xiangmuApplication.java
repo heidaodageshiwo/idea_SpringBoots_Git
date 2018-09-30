5a922eafe8df75ed6160d1407b25ce47f7f2d68a
zhangqiang <heidaodageshiwo@163.com>
2018-09-30 11:00:05 +0800

    1234com的007项目

---
+++ springboot-007xiangmu/src/main/java/com/zhangqiang/springboot007xiangmu/Springboot007xiangmuApplication.java
@@ -0,0 +1,12 @@
+package com.zhangqiang.springboot007xiangmu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot007xiangmuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot007xiangmuApplication.class, args);
+	}
+}
---
+++ springboot-007xiangmu/src/test/java/com/zhangqiang/springboot007xiangmu/Springboot007xiangmuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhangqiang.springboot007xiangmu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot007xiangmuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
